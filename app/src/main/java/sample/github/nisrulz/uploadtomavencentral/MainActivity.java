77e5e2416e2fdda9ee7d8b9b13096f6189b2d5e2
Nishant Srivastava <nisrulz@gmail.com>
2016-04-24 17:29:51 +0530

    (+) added gradle maven push plugin , gradle properties used by plugin

--- app/src/main/java/sample/github/nisrulz/uploadtomavencentral/MainActivity.java
+++ app/src/main/java/sample/github/nisrulz/uploadtomavencentral/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // Call to library functions
         ShowToast.getInstance().toast(this, "Hello World Hooman!");
     }
 }
