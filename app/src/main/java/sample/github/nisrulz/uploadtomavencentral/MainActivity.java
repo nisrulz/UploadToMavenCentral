6d0dcfa50042055bb141d11bc3e9ea976c9b24a8
Nishant Srivastava <nisrulz@gmail.com>
2019-03-05 22:42:57 +0100

    migrate to AndroidX

--- app/src/main/java/sample/github/nisrulz/uploadtomavencentral/MainActivity.java
+++ app/src/main/java/sample/github/nisrulz/uploadtomavencentral/MainActivity.java
@@ -17,7 +17,7 @@
 package sample.github.nisrulz.uploadtomavencentral;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import sample.github.nisrulz.demolibrary.ShowToast;
 
